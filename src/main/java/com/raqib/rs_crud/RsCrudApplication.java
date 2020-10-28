a9af0dcf04c6708fde7633e40bacfa3c4994a506
raqib <rhsabbir@yahoo.com>
2020-10-28 17:02:59 +0600

    first commit v1.1

--- src/main/java/com/raqib/rs_crud/RsCrudApplication.java
+++ src/main/java/com/raqib/rs_crud/RsCrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAutoConfiguration
 public class RsCrudApplication {
-
+    //application starter
 	public static void main(String[] args) {
 		SpringApplication.run(RsCrudApplication.class, args);
 	}
